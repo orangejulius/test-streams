e20cc39c6f2bc24ee305c6f9c951ae61443b0c25
Julian Simioni <julian@simioni.org>
2015-11-05 15:05:11 -0500

    Initial test of streams

---
+++ index.js
@@ -0,0 +1,19 @@
+var JSONStream = require('JSONStream');
+var fs = require('fs');
+var es = require('event-stream');
+var CombinedStream = require('combined-stream');
+
+
+ var combinedStream = CombinedStream.create();
+var fileStream = fs.createReadStream('test.json');
+var fileStream2 = fs.createReadStream('test2.json');
+
+combinedStream.append(fileStream);
+combinedStream.append(fileStream2);
+
+
+combinedStream.pipe(JSONStream.parse())
+.pipe(es.mapSync(function(data) {
+    console.log(data);
+    return data;
+}));
